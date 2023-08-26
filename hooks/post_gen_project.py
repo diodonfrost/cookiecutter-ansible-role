835d1197d7db028a3f7e3293cbb905b3ba90dd13
diodonfrost <diodon.frost@diodonfrost.me>
2023-08-27 00:21:22 +0200

    feat(cookiecutter): support licenses injection
    Previously, licenses file were not generated at the
    root project (only referenced in README.md).
    
    This commit creates LICENSE file at the root of
    the project.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -20,6 +20,7 @@ solaris_support = strtobool('{{cookiecutter.solaris_support}}')
 windows_support = strtobool('{{cookiecutter.windows_support}}')
 macos_support = strtobool('{{cookiecutter.macos_support}}')
 dev_container = strtobool('{{cookiecutter.dev_container}}')
+project_licenses = '{{cookiecutter.license}}'
 
 if not openbsd_support:
     remove(os.path.join('molecule', 'openbsd'))
@@ -42,3 +43,23 @@ if not macos_support:
 
 if not dev_container:
     remove('.devcontainer')
+
+if project_licenses == "Apache Software License 2.0":
+    shutil.move(os.path.join('_licenses', 'Apache-2'), 'LICENSE')
+    remove('_licenses')
+
+if project_licenses == "BSD-3":
+    shutil.move(os.path.join('_licenses', 'BSD-3'), 'LICENSE')
+    remove('_licenses')
+
+if project_licenses == "GNU GPL v3.0":
+    shutil.move(os.path.join('_licenses', 'GPL-3'), 'LICENSE')
+    remove('_licenses')
+
+if project_licenses == "MIT":
+    shutil.move(os.path.join('_licenses', 'MIT'), 'LICENSE')
+    remove('_licenses')
+
+if project_licenses == "Mozilla Public License 2.0":
+    shutil.move(os.path.join('_licenses', 'Mozilla-2.0'), 'LICENSE')
+    remove('_licenses')
